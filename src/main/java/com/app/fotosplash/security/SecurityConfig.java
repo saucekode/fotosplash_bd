5ac8b224ade8e6462255daeaf7f59fee44e34a17
saucekode <saucekoder@gmail.com>
2022-01-01 19:41:03 -0800

    refactoring

--- src/main/java/com/app/fotosplash/security/SecurityConfig.java
+++ src/main/java/com/app/fotosplash/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.app.fotosplash.security;
 import org.springframework.context.annotation.Configuration;;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -26,6 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/auth/**", "/oauth2/**")
                 .permitAll()
                 .anyRequest().authenticated()
+                .and().httpBasic()
                 .and().oauth2Login()
                 .authorizationEndpoint()
                 .baseUri("/oauth2/authorize")
