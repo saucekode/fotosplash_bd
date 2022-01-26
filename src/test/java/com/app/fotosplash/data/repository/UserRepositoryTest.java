11178b432fd8536ef23b5d28666a06e938e907bc
saucekode <saucekoder@gmail.com>
2022-01-25 19:56:45 -0800

    fixed user

--- src/test/java/com/app/fotosplash/data/repository/UserRepositoryTest.java
+++ src/test/java/com/app/fotosplash/data/repository/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.app.fotosplash.data.repository;
 
+import com.app.fotosplash.service.user.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
